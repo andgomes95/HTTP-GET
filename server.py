a485729cfce2d92132070178e388ef58d37d2ed5
André Gomes <andgomes95@gmail.com>
2017-10-27 12:14:40 -0200

    colocado primeiros erros

--- server.py
+++ server.py
@@ -24,22 +24,47 @@ def listagemArquivosSistema(diretorio):
                 dirr[j] = diretorio[i]+dirr[j]
             diretorio = diretorio + dirr
     return diretorio
+def printArquivo(lista):
+    for i in range(0,len(lista)):
+        lista[i] = "<br>"+lista[i]
+    return lista
 def criacaoCabecalhoServer(state,endereco,con):
     if state == "200":
         diretorio = []
         diretorio = listagemArquivosSistema(diretorio)
-
-        if not endereco in diretorio:
-            state = "401"
-            print "deu ruim"
+        if not endereco in diretorio and endereco != "/":
+            cabecalhoHTTP(200,"/404.html",con)
+        elif endereco[-1] == "/":
+            print endereco
+            arq = open(pasta+"/dir.html",'w')
+            diretorio = os.listdir(pasta+endereco)
+            arq.write("<!DOCTYPE html><html><head><meta charset=\"UTF-8\"/><title> Diretorio: "+endereco+"</title></head><body>")
+            arq.writelines(printArquivo(diretorio))
+            arq.write("</body></html>")
+            arq.close()
+            cabecalhoHTTP(200,"/dir.html",con)
         else:
             print "deu bom"
             cabecalhoHTTP(state,endereco,con)
     elif state == "400":
         print "oho"
+def contentType(arq):
+    ext = arq.split(".")
+    if ext[-1] == "html":
+        return "text/HTML"
+    elif ext[-1] == "txt":
+        return "text/txt"
+    elif ext[-1] == "jpg":
+        return "image/jpg"
+    elif ext[-1] == "png":
+        return "image/png"
+    elif ext[-1] == "gif":
+        return "image/gif"
+    elif ext[-1] == "css":
+        return "text/css"
 def cabecalhoHTTP(state,endereco,con):
     lenght = os.path.getsize(pasta+endereco)
-    saida = "HTTP/1.1 "+str(state)+" OK\nContent-Type: text/HTML\nContent-Length :"+str(lenght)+"\n\n"
+    saida = "HTTP/1.1 "+str(state)+" OK\nContent-Type: "+ contentType(endereco)+"\nContent-Length :"+str(lenght)+"\n\n"
     arq = open(pasta+endereco,'r')
     dado = arq.read()
     con.send(saida+dado)
@@ -63,33 +88,10 @@ def tratamentoCabecalhoCliente(msg):
         state = "200"
         endereco = cabecalho[0].split(" ")[1]
         print endereco
-        for i in range(1,len(cabecalho)):
-            if "Accept" == cabecalho[i].split(":")[0]:
-                for j in range(1,len(cabecalho[i].split(":"))):
-                    Accept = Accept +cabecalho[i].split(":")[j]
-                print Accept
-            if "Accept-Language" == cabecalho[i].split(":")[0]:
-                for j in range(1,len(cabecalho[i].split(":"))):
-                    AcceptLanguage = AcceptLanguage + cabecalho[i].split(":")[j]
-                print AcceptLanguage
-            if "Accept-Encoding" == cabecalho[i].split(":")[0]:
-                for j in range(1,len(cabecalho[i].split(":"))):
-                    AcceptEncoding = AcceptEncoding + cabecalho[i].split(":")[j]
-                print AcceptEncoding
-            if "User-Agent" == cabecalho[i].split(":")[0]:
-                for j in range(1,len(cabecalho[i].split(":"))):
-                    UserAgent = UserAgent + cabecalho[i].split(":")[j]
-                print UserAgent
-            if "Connection" == cabecalho[i].split(":")[0]:
-                for j in range(1,len(cabecalho[i].split(":"))):
-                    Connection = Connection + cabecalho[i].split(":")[j]
-                print Connection
-        return state,endereco,Accept,AcceptLanguage,AcceptEncoding,UserAgent,Connection
+        return state,endereco
     else:
         state = "400"
-        return state,endereco,Accept,AcceptLanguage,AcceptEncoding,UserAgent,Connection
-    #   Badrequest 400
-    #   sys.exit(0)
+        return state,endereco
 
 def conectado(con, cliente):
     print 'Conectado por', cliente
@@ -99,9 +101,8 @@ def conectado(con, cliente):
         if not msg: break
         print cliente
         print msg
-        state,endereco,accept,acceptLanguage,acceptEnconding,userAgent,connection = tratamentoCabecalhoCliente(msg)
+        state,endereco = tratamentoCabecalhoCliente(msg)
         criacaoCabecalhoServer(state,endereco,con)
-        #con.close()
 
     print 'Finalizando conexao do cliente', cliente
     con.close()
